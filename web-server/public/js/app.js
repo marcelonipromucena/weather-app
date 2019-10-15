ede1caec5a948cb3283900a2e18ebae19570e9b1
Marceloni Lima <marceloni@live.com.br>
2019-10-15 12:47:09 -0300

    Done with weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,25 @@
-console.log('Client Side javascript file is loaded.');
+console.log("Client Side javascript file is loaded.");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById("message-1");
+const messageTwo = document.getElementById("message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    },
+  );
+});
